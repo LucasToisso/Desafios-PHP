01c995bebfb13f78a8afafc9d25f901e03ee8096
LucasToisso <70295622+LucasToisso@users.noreply.github.com>
2022-05-23 00:01:14 -0300

    Tabuada usuário
    Exibindo a tabuada simulando o acesso de usuário.
    Necessário criar o localhost.

--- Tabuada/tabuada.php
+++ Tabuada/tabuada.php
@@ -1,5 +1,17 @@
-<?php
-for($num = 5; $cont <=10; $cont++){
-    echo "$num * $cont = ";
-    echo  $num * $cont .PHP_EOL;
-}
+<html>
+    <form action="" method="GET">
+
+    <input type="number" min="0" max="10" value="0" name="numero" required>
+    <button onclick="calcular()">Enter</button><br>
+
+    </form>
+
+    <?php
+    error_reporting(0);
+    $cont = 0;
+    for($num = $_GET['numero']; $cont <=10; $cont++){
+        echo "$num * $cont = ";
+        echo  $num * $cont . '<br>';
+    }
+    ?>
+</html>
